594aa41cf220ef91ec02026d117be72691227d82
刘维平 <oncwnuMk5M92OmTQZ53Oq-oCMJto@git.weixin.qq.com>
2020-05-12 16:55:23 +0800

    导出

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -2,7 +2,5 @@ import 'package:auto_construction/src/auto_generator.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 
-Builder autoBuilder(BuilderOptions options) {
-  return LibraryBuilder(AutoGenerator(),
-      generatedExtension: '.auto_construction.g.dart');
-}
+Builder autoBuilder(BuilderOptions options) => LibraryBuilder(AutoGenerator(),
+    generatedExtension: '.${DateTime.now().toIso8601String()}.g.dart');
