14a90db209597853918c0387bfeceb41b0c53434
刘维平 <oncwnuMk5M92OmTQZ53Oq-oCMJto@git.weixin.qq.com>
2020-05-12 16:20:19 +0800

    导出

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -3,5 +3,6 @@ import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 
 Builder autoBuilder(BuilderOptions options) {
-  return LibraryBuilder(AutoGenerator());
+  return LibraryBuilder(AutoGenerator(),
+      generatedExtension: '.auto_construction.g.dart');
 }
