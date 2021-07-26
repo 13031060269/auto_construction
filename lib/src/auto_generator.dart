b98833616a3b811fced360142a64fe784f5d9420
liuweiping <191706209@qq.com>
2021-07-26 18:24:12 +0800

    修改

--- lib/src/auto_generator.dart
+++ lib/src/auto_generator.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-import 'dart:collection';
 import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:auto_construction/src/auto_construction.dart';
-import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
 
 import 'create_file.dart';
 
